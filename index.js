54a4b42fb7245ee37423b6785864f60007376cb0
SALZBURG18 <lethinkg7894@gmail.com>
2024-12-06 00:24:36 +0700

    add(file): index.html style.css scripts.js files

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import express from 'express';
 const app  = express();
 const port = 9000;
 
-app.use("/", (req, res) => {
-    res.json({
-        message: "Hello From Express App!"
-    });
-});
+// app.use("/", (req, res) => {
+//     res.json({
+//         message: "Hello From Express App!"
+//     });
+// });
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 // Start server
 app.listen(port, () => {
